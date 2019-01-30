06be5a2b4fccc2f96bbe9c14791429fc153ce35a
Drian Hillman <dhillman24@gmail.com>
2019-01-30 11:53:18 -0500

    Next.js setup & introduction | Successfull npm install

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+const Home = () => {
+  return (
+    <div>
+      <p>Hey!</p>
+      <Link href='/sell'>
+        <a>Sell</a>
+      </Link>
+    </div>
+  );
+};
+
+export default Home;
