66904ed9008c8b3c42c75c0003e9c66ec47b871d
Drian Hillman <dhillman24@gmail.com>
2019-01-31 16:25:17 -0500

    Creates & deploys application GraphQL Yoga server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,24 @@
-// let's go!
+require('dotenv').config({ path: 'variables.env' });
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO: Use express middleware to handle cookies (JWT)
+// TODO: Use express middleware to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`\n🏀  Welcome to the Jump!`);
+    console.log(
+      '\x1b[36m%s\x1b[0m',
+      `\nServer is now running on Port ${deets.port}:\n    http://localhost:${deets.port}\n`
+    );
+  }
+);
