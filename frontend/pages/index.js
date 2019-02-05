709a998d19753e6ff81ccb969482f0ec2a5d1dcf
Drian Hillman <dhillman24@gmail.com>
2019-02-05 12:24:12 -0500

    Creates the page to display single items

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,10 @@
 import Items from '../components/Items';
 
-const Home = () => {
-  return (
-    <>
-      <h3>Home</h3>
-      <Items />
-    </>
-  );
-};
+const Home = props => (
+  <div>
+    <h3>Home</h3>
+    <Items />
+  </div>
+);
 
 export default Home;
