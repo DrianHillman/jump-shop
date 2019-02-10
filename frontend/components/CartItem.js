cd76dfd873af71a394ae41e3d1c31602e0aca200
Drian Hillman <dhillman24@gmail.com>
2019-02-10 12:32:07 -0500

    Dealing with Deleted store items that are in a User's cart

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -19,22 +19,32 @@ const CartItemStyles = styled.li`
   }
 `;
 
-const CartItem = ({ cartItem }) => (
-  <CartItemStyles>
-    <img src={cartItem.item.image} alt={cartItem.item.title} width='100' />
-    <div className='cart-item-details'>
-      <h3>{cartItem.item.title}</h3>
-      <p>
-        {formatMoney(cartItem.item.price)}
-        {' - '}
-        <em>
-          {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
-        </em>
-      </p>
-    </div>
-    <RemoveFromCart id={cartItem.id} />
-  </CartItemStyles>
-);
+const CartItem = ({ cartItem }) => {
+  // first check if that item exists
+  if (!cartItem.item)
+    return (
+      <CartItemStyles>
+        <p>This item has been removed.</p>
+        <RemoveFromCart id={cartItem.id} />
+      </CartItemStyles>
+    );
+  return (
+    <CartItemStyles>
+      <img src={cartItem.item.image} alt={cartItem.item.title} width='100' />
+      <div className='cart-item-details'>
+        <h3>{cartItem.item.title}</h3>
+        <p>
+          {formatMoney(cartItem.item.price)}
+          {' - '}
+          <em>
+            {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} each
+          </em>
+        </p>
+      </div>
+      <RemoveFromCart id={cartItem.id} />
+    </CartItemStyles>
+  );
+};
 
 CartItem.prototypes = {
   cartItem: PropTypes.object.isRequired,
