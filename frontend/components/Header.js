de4b0fbea3753e82b2dd5a4b4c497e04e6e0dcd9
Drian Hillman <dhillman24@gmail.com>
2019-02-24 23:32:13 -0500

    Site logo change

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -64,7 +64,7 @@ const Header = () => (
     <div className='bar'>
       <Logo>
         <Link href='/'>
-          <a>Jump Shop!</a>
+          <a>The Jump Shop</a>
         </Link>
       </Logo>
       <Nav />
