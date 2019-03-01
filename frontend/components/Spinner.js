4f8892bba7d35a3122ac1da2a338669b25b377bd
Drian Hillman <dhillman24@gmail.com>
2019-02-28 21:55:50 -0500

    Update spinner spacing

--- frontend/components/Spinner.js
+++ frontend/components/Spinner.js
@@ -3,7 +3,14 @@ import MDSpinner from 'react-md-spinner';
 
 const Spinner = () => {
   return (
-    <MDSpinner color1='#FF0000' color2='#3A3A3A' color3='#FF0000' color4='#3A3A3A' size={72} />
+    <MDSpinner
+      color1='#FF0000'
+      color2='#3A3A3A'
+      color3='#FF0000'
+      color4='#3A3A3A'
+      size={72}
+      style={{ marginTop: '4rem' }}
+    />
   );
 };
 
